0a544d3b82df49c3413c483d25018682d6f37812
adityagupta99826@gmail.com <adityagupta99826@gmail.com>
2025-03-07 10:25:57 +0530

    7 march

---
+++ AIML/back/front-back/back/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+app.use(cors());//builtin middleware
+
+app.get("/api",async(req,res)=>{
+    const apidata = await axios.get("https://dummyjson.com/products");
+    res.send(apidata.data.products);
+});
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
